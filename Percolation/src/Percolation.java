f1aaa9ca8044e53decf09851d290fdf23e636645
Liwink <lyh.next@gmail.com>
2016-11-23 13:50:56 +0800

    add Percolation

---
+++ Percolation/src/Percolation.java
@@ -0,0 +1,140 @@
+//import java.lang.*;
+
+import edu.princeton.cs.algs4.StdRandom;
+
+public class Percolation {
+    private boolean[] opened;
+    private int[] id;
+    private int n;
+    private int[] size;
+
+
+    public Percolation(int n) {
+        if (n <= 0) {
+            throw new java.lang.IllegalArgumentException();
+        }
+
+        this.n = n;
+        opened = new boolean[n * n];
+        id = new int[n * n];
+        size = new int[n * n];
+
+        int index;
+
+        for (int j = 1; j <= n; j++) {
+            for (int i = 1; i <= n; i++) {
+                index = this.getId(i, j);
+                if (j == 1) {
+                    id[index] = 0;
+                } else if (j == n) {
+                    id[index] = n * n - 1;
+                } else {
+                    id[index] = index;
+                    size[index] = 1;
+                }
+                opened[index] = false;
+            }
+        }
+        size[0] = n;
+        size[n * n - 1] = n;
+    }
+
+    private boolean check_valid(int row, int col) {
+        return row > 0 && row <= n && col <= n && col > 0;
+    }
+
+    private int getId(int row, int col) {
+        if (!check_valid(row, col)) {
+            System.out.println("error:" + row + col);
+            throw new java.lang.IndexOutOfBoundsException();
+        }
+        return row + n * (col - 1) - 1;
+    }
+
+    public void open(int row, int col) {
+        if (!check_valid(row, col)) {
+            throw new java.lang.IndexOutOfBoundsException();
+        }
+        opened[getId(row, col)] = true;
+        if (check_valid(row - 1, col - 1) && isOpen(row - 1, col - 1)) {
+            union(row, col, row - 1, col - 1);
+        }
+        if (check_valid(row + 1, col - 1) && isOpen(row + 1, col - 1)) {
+            union(row, col, row + 1, col - 1);
+        }
+        if (check_valid(row + 1, col + 1) && isOpen(row + 1, col + 1)) {
+            union(row, col, row + 1, col + 1);
+        }
+        if (check_valid(row - 1, col + 1) && isOpen(row - 1, col + 1)) {
+            union(row, col, row - 1, col + 1);
+        }
+    }
+
+    public boolean isOpen(int row, int col) {
+        if (!check_valid(row, col)) {
+            throw new java.lang.IndexOutOfBoundsException();
+        }
+        return opened[getId(row, col)];
+    }
+
+    public boolean isFull(int row, int col) {
+        if (!check_valid(row, col)) {
+            throw new java.lang.IndexOutOfBoundsException();
+        }
+        return (root(row, col) == root(1, 1));
+    }
+
+    public boolean percolates() {
+        return isFull(n, n);
+    }
+
+    private int root(int row, int col) {
+        int i = getId(row, col);
+        while (i != id[i]) {
+            id[i] = id[id[i]];
+            i = id[i];
+        }
+        return i;
+    }
+
+    private void union(int row1, int col1, int row2, int col2) {
+        if (!check_valid(row1, col1)) {
+            throw new java.lang.IndexOutOfBoundsException();
+        }
+        if (!check_valid(row2, col2)) {
+            throw new java.lang.IndexOutOfBoundsException();
+        }
+
+        int root1 = root(row1, col1);
+        int root2 = root(row2, col2);
+        if (root1 == root2) return;
+
+        if (size[root1] > size[root2]) {
+            id[root2] = root1;
+            size[root1] += size[root2];
+        } else {
+            id[root1] = root2;
+            size[root2] += size[root1];
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.print("start!");
+        Percolation p = new Percolation(100);
+
+        int i = 0;
+
+        while (!p.percolates()) {
+            int row = StdRandom.uniform(100) + 1;
+            int col = StdRandom.uniform(100) + 1;
+            if (p.isOpen(row, col)) {
+                continue;
+            }
+            i += 1;
+            p.open(row, col);
+        }
+        System.out.println("total count: " + Integer.toString(i));
+    }
+
+}
+
---
+++ Percolation/src/PercolationStats.java
@@ -0,0 +1,74 @@
+/**
+ * Created by Liwink on 11/23/16.
+ */
+
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class PercolationStats {
+    private int n;
+    private double[] results;
+    private double mean;
+    private double stddev;
+    private int trials;
+
+    public PercolationStats(int num, int trials) {
+        if (n <= 0 || trials <= 0) {
+            throw new java.lang.IllegalArgumentException();
+        }
+        n = num;
+        this.trials = trials;
+
+        double total = n * n;
+
+        results = new double[trials];
+
+        for (int i = 0; i < trials; i++) {
+
+            Percolation p = new Percolation(n);
+
+            int count = 0;
+
+            while (!p.percolates()) {
+                int row = StdRandom.uniform(100);
+                int col = StdRandom.uniform(100);
+                if (p.isOpen(row, col)) {
+                    continue;
+                }
+                count += 1;
+                p.open(row, col);
+            }
+            results[i] = count / total;
+        }
+
+        mean = this.mean();
+        stddev = this.stddev();
+    }
+
+    public double mean() {
+        return StdStats.mean(results);
+    }
+
+    public double stddev() {
+        return StdStats.stddev(results);
+    }
+
+    public double confidenceLo() {
+        return mean - 1.96 * stddev / Math.sqrt(trials);
+    }
+
+    public double confidenceHi() {
+        return mean + 1.96 * stddev / Math.sqrt(trials);
+    }
+
+    public static void main(String[] args) {
+        int num = Integer.parseInt(args[0]);
+        int t = Integer.parseInt(args[1]);
+
+        PercolationStats p = new PercolationStats(num, t);
+        System.out.println("mean = " + Double.toString(p.mean()));
+        System.out.println("stddev = " + Double.toString(p.stddev()));
+        System.out.println("95% confidence interval = " + Double.toString(p.confidenceLo())
+                + ", " + Double.toString(p.confidenceHi()));
+    }
+}
