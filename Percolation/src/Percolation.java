10d7cfee509e7751b3cbac3698304e13701c2e60
Liwink <lyh.next@gmail.com>
2016-11-23 22:21:42 +0800

    refine Percolation: max[] to help!

--- Percolation/src/Percolation.java
+++ Percolation/src/Percolation.java
@@ -3,10 +3,9 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
     private boolean[] opened;
-    private int[] id;
     private int n;
-    private int[] size;
     private WeightedQuickUnionUF un;
+    private int[] max;
 
 
     public Percolation(int n) {
@@ -17,8 +16,6 @@ public class Percolation {
         this.n = n;
 //        The default value for the elements in a boolean[] is false.
         opened = new boolean[n * n];
-        id = new int[n * n];
-        size = new int[n * n];
 
         un = new WeightedQuickUnionUF(n * n);
 
@@ -26,6 +23,10 @@ public class Percolation {
             un.union(0, getId(1, i));
         }
 
+        max = new int[n * n];
+        for (int i = 0; i < n * n; i++) {
+            max[i] = i;
+        }
     }
 
     private boolean checkValid(int row, int col) {
@@ -48,17 +49,24 @@ public class Percolation {
         opened[getId(row, col)] = true;
         int index = getId(row, col);
         if (checkValid(row, col - 1) && isOpen(row, col - 1)) {
-            un.union(index, getId(row, col - 1));
+            union(index, getId(row, col - 1));
         }
         if (checkValid(row, col + 1) && isOpen(row, col + 1)) {
-            un.union(index, getId(row, col + 1));
+            union(index, getId(row, col + 1));
         }
         if (checkValid(row + 1, col) && isOpen(row + 1, col)) {
-            un.union(index, getId(row + 1, col));
+            union(index, getId(row + 1, col));
         }
         if (checkValid(row - 1, col) && isOpen(row - 1, col)) {
-            un.union(index, getId(row - 1, col));
+            union(index, getId(row - 1, col));
         }
+    }
+
+    private void union(int p, int q) {
+        int a = un.find(p);
+        int b = un.find(q);
+        un.union(p, q);
+        max[un.find(p)] = Math.max(max[a], max[b]);
     }
 
     public boolean isOpen(int row, int col) {
@@ -76,12 +84,8 @@ public class Percolation {
     }
 
     public boolean percolates() {
-        for (int i = 1; i <= n; i++){
-            if (isFull(n, i)) {
-                return true;
-            }
-        }
-        return false;
+//        System.out.println(max[un.find(0)]);
+        return max[un.find(0)] > n * (n - 1) - 1;
     }
 
     public static void main(String[] args) {
@@ -90,25 +94,28 @@ public class Percolation {
         Percolation pt = new Percolation(50);
         System.out.println(pt.isFull(1, 48) + " sure be false");
         pt.open(1, 48);
+        pt.percolates();
         System.out.println(pt.isFull(1, 48) + " sure be true");
         pt.open(3, 48);
+        pt.percolates();
         System.out.println(pt.isFull(3, 48) + " sure be false");
         pt.open(2, 48);
+        pt.percolates();
         System.out.println(pt.isFull(2, 48) + " sure be true");
         System.out.println(pt.isFull(3, 48) + " sure be true");
 
-        Percolation p = new Percolation(100);
-        int i = 0;
-        while (!p.percolates()) {
-            int row = StdRandom.uniform(100) + 1;
-            int col = StdRandom.uniform(100) + 1;
-            if (p.isOpen(row, col)) {
-                continue;
-            }
-            i += 1;
-            p.open(row, col);
-        }
-        System.out.println("total count: " + Integer.toString(i));
+//        Percolation p = new Percolation(100);
+//        int i = 0;
+//        while (!p.percolates()) {
+//            int row = StdRandom.uniform(100) + 1;
+//            int col = StdRandom.uniform(100) + 1;
+//            if (p.isOpen(row, col)) {
+//                continue;
+//            }
+//            i += 1;
+//            p.open(row, col);
+//        }
+//        System.out.println("total count: " + Integer.toString(i));
     }
 
 }
