2294153864766faaed86bd3db343f1cf2bdee487
Liwink <lyh.next@gmail.com>
2016-12-03 21:10:52 +0800

    more effective to deal more than 4 points on line by PointPair

--- collinear/src/FastCollinearPoints.java
+++ collinear/src/FastCollinearPoints.java
@@ -1,12 +1,11 @@
 import java.util.Arrays;
+import java.util.Comparator;
 
 import edu.princeton.cs.algs4.In;
-import edu.princeton.cs.algs4.Queue;
 import edu.princeton.cs.algs4.Stack;
 import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdOut;
 
-import javax.swing.text.Position;
 
 /**
  * Created by Liwink on 12/2/16.
@@ -14,10 +13,41 @@ import javax.swing.text.Position;
 
 public class FastCollinearPoints {
     private Stack<LineSegment> lineSegments = new Stack<LineSegment>();
-    private Queue<Point> storedPoints = new Queue<Point>();
     private Point[] points;
+    private Stack<PointPair> pointPairs = new Stack<PointPair>();
 
     private int COUNT = 2;
+
+    private class PointPair {
+        public Point p1;
+        public Point p2;
+        public final Comparator<PointPair> BY_SLOPE = new BySlope();
+
+        public PointPair(Point p1, Point p2) {
+            this.p1 = p1;
+            this.p2 = p2;
+        }
+
+        private class BySlope implements Comparator<PointPair> {
+            public int compare(PointPair pr1, PointPair pr2) {
+                double s1 = pr1.p1.slopeTo(pr1.p2);
+                double s2 = pr2.p1.slopeTo(pr2.p2);
+                if (s1 > s2) return 1;
+                else if (s1 < s2) return -1;
+                else {
+                    int j = pr1.p2.compareTo(pr2.p2);
+                    if (j == 0) return pr1.p1.compareTo(pr2.p1);
+                    return j;
+                }
+            }
+        }
+
+        public boolean sameLine(PointPair pp) {
+            if (this.p1.slopeTo(this.p2) == pp.p1.slopeTo(pp.p2)
+                    && this.p2.compareTo(pp.p2) == 0) return true;
+            else return false;
+        }
+    }
 
     public FastCollinearPoints(Point[] ps) {
         int len = ps.length;
@@ -26,7 +56,7 @@ public class FastCollinearPoints {
 
         Arrays.sort(points);
         for (int i = 1; i < len; i++) {
-            if (points[i].compareTo(points[i-1]) == 0) throw new IllegalArgumentException();
+            if (points[i].compareTo(points[i - 1]) == 0) throw new IllegalArgumentException();
         }
 
         Point[] resortPoints = new Point[len];
@@ -50,20 +80,23 @@ public class FastCollinearPoints {
                     currentCount--;
                     boolean check = true;
                     if (currentCount <= 0) {
-                        for (Point l : storedPoints) {
-                            if (l.slopeTo(resortPoints[i]) == slope) {
-                                check = false;
-                                break;
-                            }
-                        }
-                        if (check) {
-                            if (currentCount < 0) lineSegments.pop();
-                            lineSegments.push(new LineSegment(resortPoints[i], resortPoints[j]));
-                            storedPoints.enqueue(resortPoints[i]);
-                        }
+                        if (currentCount < 0) pointPairs.pop();
+                        pointPairs.push(new PointPair(resortPoints[i], resortPoints[j]));
                     }
                 }
             }
+        }
+        PointPair[] pairList = new PointPair[pointPairs.size()];
+        int i = 0;
+        for (PointPair p :
+                pointPairs) {
+            pairList[i] = p;
+            i++;
+        }
+        Arrays.sort(pairList, new PointPair(new Point(1, 2), new Point(2, 3)).BY_SLOPE);
+        for (int j = 0; j < pointPairs.size(); j++) {
+            if (j > 0 && pairList[j].sameLine(pairList[j - 1])) continue;
+            lineSegments.push(new LineSegment(pairList[j].p1, pairList[j].p2));
         }
     }
 
