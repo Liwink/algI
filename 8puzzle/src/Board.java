74380e22e7d68856f7dd963f04ac457849b23353
Liwink <lyh.next@gmail.com>
2016-12-06 17:15:58 +0800

    fix getManhattan

--- 8puzzle/src/Board.java
+++ 8puzzle/src/Board.java
@@ -134,8 +134,9 @@ public class Board {
 
     private int getManhattan(int row, int col, int point) {
         if (point == 0) return 0;
+        point--;
         int rowP = point / dim;
-        int colP = point - rowP * dim - 1;
+        int colP = point - rowP * dim;
         return Math.abs(row - rowP) + Math.abs(col - colP);
     }
 
